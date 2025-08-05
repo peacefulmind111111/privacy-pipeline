b0d07e445bfebe4be49e5f74dd02ecb0a29bf223
lexcion <juh020@ucsd.edu>
2025-08-04 20:53:16 -0700

    reset

--- tests/test_data.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from dp_pipeline import get_cifar10_dataloaders
-
-
-def test_get_cifar10_dataloaders_requires_torch():
-    with pytest.raises(RuntimeError):
-        get_cifar10_dataloaders(batch_size=4)
